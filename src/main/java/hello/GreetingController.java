76a41e09ca39ee2eb787c76e502599a3a41ad143
Mick Gallagher <mickster.gallagher@gmail.com>
2014-05-09 16:17:25 -0700

    Tweaking the controller

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,8 @@
 package hello;
 
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,13 +11,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, %s! Today is %s";
     private final AtomicLong counter = new AtomicLong();
 
+    /***************************************************************
+     * @param name
+     * @return
+     ***************************************************************/
     @RequestMapping("/greeting")
     public @ResponseBody Greeting greeting(
-            @RequestParam(value = "name", required = false, defaultValue = "Billybob") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template,
-                name));
+        @RequestParam(value = "name", required = false, defaultValue = "Billybob") String name) {
+
+        Date today = new Date();
+        Greeting g = new Greeting(
+            counter.incrementAndGet(),
+            String.format(template, name, today));
+
+        return g;
     }
 }
