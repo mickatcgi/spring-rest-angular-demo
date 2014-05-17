3d3cc6b1f768878eb83222734f0d02db79f57724
Mick Gallagher <mickster.gallagher@gmail.com>
2014-05-16 18:03:04 -0700

    Monkeying with angular-ui widgets

--- src/main/webapp/public/js/controllers.js
+++ src/main/webapp/public/js/controllers.js
@@ -1,6 +1,8 @@
 'use strict';
 
-/* Controllers - configure interaction with back-end */
+/*******************************************************************************
+ * Controllers - configure interaction with back-end
+ ******************************************************************************/
 
 var trade360Controllers = angular.module('trade360Controllers', []);
 
@@ -9,32 +11,66 @@ var instr_url = base_url + "instrument.json";
 var wi_url = base_url + "workItem.json";
 var cust_url = base_url + "customer.json";
 
-trade360Controllers.controller('instrumentCtrl', [
-		'$scope',
-		'$http',
+trade360Controllers.controller('instrumentCtrl', [ '$scope', '$http',
 		function($scope, $http) {
-			$http.get(instr_url).success(
-					function(data) {
-						$scope.instrument = data;
-					});
+			$http.get(instr_url).success(function(data) {
+				$scope.instrument = data;
+			});
 		} ]);
 
-trade360Controllers.controller('workItemCtrl', [
-		'$scope',
-		'$http',
+trade360Controllers.controller('workItemCtrl', [ '$scope', '$http',
 		function($scope, $http) {
-			$http.get(wi_url).success(
-					function(data) {
-						$scope.workItem = data;
-					});
+			$http.get(wi_url).success(function(data) {
+				$scope.workItem = data;
+			});
 		} ]);
 
-trade360Controllers.controller('customerCtrl', [
-		'$scope',
-		'$http',
+trade360Controllers.controller('customerCtrl', [ '$scope', '$http',
 		function($scope, $http) {
-			$http.get(cust_url).success(
-					function(data) {
-						$scope.customer = data;
-					});
+			$http.get(cust_url).success(function(data) {
+				$scope.customer = data;
+			});
 		} ]);
+
+/*******************************************************************************
+ * Button Controller
+ ******************************************************************************/
+
+angular.module('myModule', [ 'ui.bootstrap' ]);
+
+var ButtonsCtrl = function($scope) {
+
+	$scope.singleModel = 1;
+
+	$scope.radioModel = 'Middle';
+
+	$scope.checkModel = {
+		left : false,
+		middle : true,
+		right : false
+	};
+};
+
+/*******************************************************************************
+ * Alert Controller
+ ******************************************************************************/
+function AlertDemoCtrl($scope) {
+	$scope.alerts = [ {
+		type : 'danger',
+		msg : 'Alert Danger'
+	}, {
+		type : 'success',
+		msg : 'Alert Success'
+	} ];
+
+	$scope.addAlert = function() {
+		$scope.alerts.push({
+			msg : 'Another alert!'
+		});
+	};
+
+	$scope.closeAlert = function(index) {
+		$scope.alerts.splice(index, 1);
+	};
+
+}
