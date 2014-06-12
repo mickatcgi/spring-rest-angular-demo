26aaa3d6fb3f6e3f115cf476616ec27fb15c8516
Mick Gallagher <mickster.gallagher@gmail.com>
2014-06-11 18:57:30 -0700

    Shuffling methods and comments to make the controller more readable

--- src/main/webapp/public/js/controllers.js
+++ src/main/webapp/public/js/controllers.js
@@ -24,67 +24,73 @@ trade360Controllers.controller('UserController',
     ['$scope', '$routeParams', 'UserFactory', '$location', '$route',
         function ($scope, $routeParams, UserFactory, $location, $route) {
 
-            // callback for ng-click 'editUser' - launch user-edit page
-            $scope.editUser = function (userId) {
-                console.log("MICK - editUser() editing user with id = " + userId);
-                $location.path('/user-edit/' + userId);
-            };
-
-            // callback for ng-click 'updateUser':
-            $scope.updateUser = function (userId) {
-                UserFactory.update({ id: userId}, $scope.user).$promise.then(function(result) {
-                    console.log("MICK updateUser promise returned = " + JSON.stringify(result));
-                    $location.path('/user-list');
-                });
-            };
-
-            // callback for ng-click 'showUser' - launch user-show page
-            $scope.showUser = function (userId) {
-                console.log("MICK - showUser() showing user with id = " + userId);
-                $location.path('/user-show/' + userId);
-            };
-
-            // callback for ng-click 'newUser' - launch user-create page
-            $scope.newUser = function () {
-                $location.path('/user-create');
-            };
-
-            // callback for ng-click 'createNewUser':
-            $scope.createNewUser = function () {
-                UserFactory.create($scope.user).$promise.then(function(result) {
-                    console.log("MICK createUser promise returned = " + JSON.stringify(result));
-                    $location.path('/user-list');
-                });
-            }
-
-            // callback for ng-click 'deleteUser' - delete and redisplay list
-            $scope.deleteUser = function (userId) {
-                UserFactory.delete({ id: userId }).$promise.then(function(result) {
-                    console.log("MICK delete promise returned = " + JSON.stringify(result));
-
-                    // When invoked from show user page this causes list GET to
-                    // be executed twice but from main user list page GET is
-                    // only invoked once.
-                    $route.reload();
-
-                    $location.path('/user-list');
-                });
-            };
-
-            // callback for auto-load 'listUsers' - display list
-            // Method invoked on page load from data-ng-init call
+            // Lists all the users. Method invoked on page load from data-ng-init call
             $scope.listUsers = function () {
                 console.log("MICK - listUsers() getting list of users");
                 $scope.users = UserFactory.query();
             };
 
-            // General get function used by pages that GET a single user
+            // Launches the show-user page to display full user details
+            $scope.showUser = function (userId) {
+                console.log("MICK - showUser() showing user with id = " + userId);
+                $location.path('/user-show/' + userId);
+            };
+
+            // Launches the edit page in preparation for updating an existing
+            // users details.
+            $scope.editUser = function (userId) {
+                console.log("MICK - editUser() editing user with id = " + userId);
+                $location.path('/user-edit/' + userId);
+            };
+
+            // Performs the update operation on an existing user
+            $scope.updateUser = function (userId) {
+                UserFactory.update({ id: userId}, $scope.user).$promise.then(function (result) {
+                    console.log("MICK updateUser promise returned = " + JSON.stringify(result));
+                    $location.path('/user-list');
+                });
+            };
+
+            // Launches the user-create page in preparation for entering new
+            // user details.
+            $scope.newUser = function () {
+                $location.path('/user-create');
+            };
+
+            // Performs the create operation a new user after entering all new user details
+            $scope.createNewUser = function () {
+                UserFactory.create($scope.user).$promise.then(function (result) {
+                    console.log("MICK createUser promise returned = " + JSON.stringify(result));
+                    $location.path('/user-list');
+                });
+            }
+
+            // Delete a user and return to the user-list or redisplay the user-list
+            // if already on that page.
+            $scope.deleteUser = function (userId) {
+                UserFactory.delete({ id: userId }).$promise.then(function (result) {
+                    console.log("MICK delete promise returned = " + JSON.stringify(result));
+
+                    // We really only need to do a reload if we're on the user-list
+                    // page since it doesn't reload automatically so after a delete
+                    // the list never gets refreshed. Deletes from other pages
+                    // work ok because they trigger a user-list reload by default
+                    if ($location.path() == '/user-list') {
+                        $route.reload();
+                    }
+
+                    $location.path('/user-list');
+                });
+            };
+
+            // General get function used by pages that GET a single user, e.g.
+            // edit-user and show-user pages
             $scope.getUser = function () {
                 console.log("MICK - getUser() getting one user. id = " + $routeParams.id);
                 $scope.user = UserFactory.show({ id: $routeParams.id});
             };
 
-            // callback for ng-click 'cancel':
+            // Cancels an edit or a show action
             $scope.cancel = function () {
                 $location.path('/user-list');
             };
